bb09de4077dbc37632003f16ab6d7ca6170b0816
kostat <kostat@gmail.com>
2016-01-15 17:04:04 +0200

    support sending Application properties

--- src/main/java/com/microsoft/eventhubs/client/EventHubSender.java
+++ src/main/java/com/microsoft/eventhubs/client/EventHubSender.java
@@ -17,6 +17,10 @@
  *******************************************************************************/
 package com.microsoft.eventhubs.client;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
 import java.util.concurrent.TimeoutException;
 
 import org.apache.qpid.amqp_1_0.client.LinkDetachedException;
@@ -25,81 +29,93 @@ import org.apache.qpid.amqp_1_0.client.Sender;
 import org.apache.qpid.amqp_1_0.client.Session;
 import org.apache.qpid.amqp_1_0.type.Binary;
 import org.apache.qpid.amqp_1_0.type.Section;
+import org.apache.qpid.amqp_1_0.type.messaging.ApplicationProperties;
 import org.apache.qpid.amqp_1_0.type.messaging.Data;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class EventHubSender {
 
-  private static final Logger logger = LoggerFactory.getLogger(EventHubSender.class);
+	private static final Logger logger = LoggerFactory.getLogger(EventHubSender.class);
 
-  private final Session session;
-  private final String entityPath;
-  private final String partitionId;
-  private final String destinationAddress;
+	private final Session session;
+	private final String entityPath;
+	private final String partitionId;
+	private final String destinationAddress;
 
-  private Sender sender;
+	private Sender sender;
 
-  public EventHubSender(Session session, String entityPath, String partitionId) {
-    this.session = session;
-    this.entityPath = entityPath;
-    this.partitionId = partitionId;
-    this.destinationAddress = getDestinationAddress();
-  }
-  
-  public void send(Section section) throws EventHubException {
-    try {
-      if (sender == null) {
-        ensureSenderCreated();
-      }
+	public EventHubSender(Session session, String entityPath, String partitionId) {
+		this.session = session;
+		this.entityPath = entityPath;
+		this.partitionId = partitionId;
+		this.destinationAddress = getDestinationAddress();
+	}
 
-      Message message = new Message(section);
-      sender.send(message);
+	public void send(Section section) throws EventHubException {
+		send(Collections.singletonList(section));
+	}
 
-    } catch (LinkDetachedException e) {
-      logger.error(e.getMessage());
+	public void send(Collection<Section> sections) throws EventHubException {
+		try {
+			if (sender == null) {
+				ensureSenderCreated();
+			}
 
-      EventHubException eventHubException = new EventHubException("Sender has been closed");
-      throw eventHubException;
-    } catch (TimeoutException e) {
-      logger.error(e.getMessage());
+			Message message = new Message(sections);
+			sender.send(message);
 
-      EventHubException eventHubException = new EventHubException("Timed out while waiting to get credit to send");
-      throw eventHubException;
-    } catch (Exception e) {
-      logger.error(e.getMessage());
-    }
-  }
-  
-  public void send(byte[] data) throws EventHubException {
-	  Binary bin = new Binary(data);
-	  send(new Data(bin));
-  }
+		} catch (LinkDetachedException e) {
+			logger.error(e.getMessage());
 
-  public void send(String data) throws EventHubException {
-    //For interop with other language, convert string to bytes
-    send(data.getBytes());
-  }
+			EventHubException eventHubException = new EventHubException("Sender has been closed");
+			throw eventHubException;
+		} catch (TimeoutException e) {
+			logger.error(e.getMessage());
 
-  public void close() {
-    try {
-      sender.close();
-    } catch (Sender.SenderClosingException e) {
-      logger.error("Closing a sender encountered error: " + e.getMessage());
-    }
-  }
+			EventHubException eventHubException = new EventHubException("Timed out while waiting to get credit to send");
+			throw eventHubException;
+		} catch (Exception e) {
+			logger.error(e.getMessage());
+		}
+	}
 
-  private String getDestinationAddress() {
-    if (partitionId == null || partitionId.equals("")) {
-      return entityPath;
-    } else {
-      return String.format(Constants.DestinationAddressFormatString, entityPath, partitionId);
-    }
-  }
+	public void send(byte[] data, Map<String, Object> properties) throws EventHubException {
+		Binary bin = new Binary(data);
+		Data dataSection = new Data(bin);
 
-  private synchronized void ensureSenderCreated() throws Exception {
-    if (sender == null) {
-      sender = session.createSender(destinationAddress);
-    }
-  }
+		if (properties == null || properties.size() == 0)
+			send(dataSection);
+		else {
+			ApplicationProperties applicationProperties = new ApplicationProperties(properties);
+			send(Arrays.asList(dataSection, applicationProperties));
+		}
+	}
+
+	public void send(String data, Map<String, Object> properties) throws EventHubException {
+		// For interop with other language, convert string to bytes
+		send(data.getBytes(), properties);
+	}
+
+	public void close() {
+		try {
+			sender.close();
+		} catch (Sender.SenderClosingException e) {
+			logger.error("Closing a sender encountered error: " + e.getMessage());
+		}
+	}
+
+	private String getDestinationAddress() {
+		if (partitionId == null || partitionId.equals("")) {
+			return entityPath;
+		} else {
+			return String.format(Constants.DestinationAddressFormatString, entityPath, partitionId);
+		}
+	}
+
+	private synchronized void ensureSenderCreated() throws Exception {
+		if (sender == null) {
+			sender = session.createSender(destinationAddress);
+		}
+	}
 }
--- src/main/java/com/microsoft/eventhubs/client/example/EventHubSendClient.java
+++ src/main/java/com/microsoft/eventhubs/client/example/EventHubSendClient.java
@@ -25,50 +25,52 @@ import com.microsoft.eventhubs.client.EventHubSender;
  * An example showing how to use EventHubSender to send messages to EventHubs
  */
 public class EventHubSendClient {
-  
-  public static void main(String[] args) {
-    
-    if (args == null || args.length < 7) {
-      System.out.println("Usage: SendClient <policyName> <policyKey> <namespace> <name> <partitionId> <messageSize> <messageCount>");
-      return;
-    }
-    
-    String policyName = args[0];
-    String policyKey = args[1];
-    String namespace = args[2];
-    String name = args[3];
-    String partitionId = args[4];
-    int messageSize = Integer.parseInt(args[5]);
-    int messageCount = Integer.parseInt(args[6]);
-    assert(messageSize > 0);
-    assert(messageCount > 0);
-    
-    if (partitionId.equals("-1")) {
-      // -1 means we want to send data to partitions in round-robin fashion.
-      partitionId = null;
-    }
-    
-    try {
-      EventHubClient client = EventHubClient.create(policyName, policyKey, namespace, name);
-      EventHubSender sender = client.createPartitionSender(partitionId);
-      
-      StringBuilder sb = new StringBuilder(messageSize);
-      for(int i=1; i<messageCount+1; ++i) {
-        while(sb.length() < messageSize) {
-          sb.append(" current message: " + i);
-        }
-        sb.setLength(messageSize);
-        sender.send(sb.toString());
-        sb.setLength(0);
-        if(i % 1000 == 0) {
-          System.out.println("Number of messages sent: " + i);
-        }
-      }
-      System.out.println("Total Number of messages sent: " + messageCount);
-    } catch (EventHubException e) {
-      System.out.println("Exception: " + e.getMessage());
-    }
-    
-    System.out.println("done");
-  }
+
+	public static void main(String[] args) {
+
+		if (args == null || args.length < 7) {
+			System.out
+					.println("Usage: SendClient <policyName> <policyKey> <namespace> <name> <partitionId> <messageSize> <messageCount>");
+			return;
+		}
+
+		String policyName = args[0];
+		String policyKey = args[1];
+		String namespace = args[2];
+		String name = args[3];
+		String partitionId = args[4];
+		int messageSize = Integer.parseInt(args[5]);
+		int messageCount = Integer.parseInt(args[6]);
+		assert (messageSize > 0);
+		assert (messageCount > 0);
+
+		if (partitionId.equals("-1")) {
+			// -1 means we want to send data to partitions in round-robin
+			// fashion.
+			partitionId = null;
+		}
+
+		try {
+			EventHubClient client = EventHubClient.create(policyName, policyKey, namespace, name);
+			EventHubSender sender = client.createPartitionSender(partitionId);
+
+			StringBuilder sb = new StringBuilder(messageSize);
+			for (int i = 1; i < messageCount + 1; ++i) {
+				while (sb.length() < messageSize) {
+					sb.append(" current message: " + i);
+				}
+				sb.setLength(messageSize);
+				sender.send(sb.toString(), null);
+				sb.setLength(0);
+				if (i % 1000 == 0) {
+					System.out.println("Number of messages sent: " + i);
+				}
+			}
+			System.out.println("Total Number of messages sent: " + messageCount);
+		} catch (EventHubException e) {
+			System.out.println("Exception: " + e.getMessage());
+		}
+
+		System.out.println("done");
+	}
 }
